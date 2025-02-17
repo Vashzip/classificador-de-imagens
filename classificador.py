a54e558f9bb19e88d2652523df1152d6d0f0c3d5
Vash <vashgithub@gmail.com>
2025-02-17 14:01:59 -0300

    Update classificador.py

--- classificador.py
+++ classificador.py
@@ -2,11 +2,9 @@ import torch
 
 print("Versão do PyTorch:", torch.__version__)
 
-# Teste básico: cria um tensor aleatório
 x = torch.rand(3, 3)
 print("Tensor aleatório:\n", x)
 
-# Teste de operação simples
 a = torch.tensor([2.0, 3.0])
 b = torch.tensor([4.0, 5.0])
 print("Soma de tensores:", a + b)
